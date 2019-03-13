c3697ef1200a104a5b8fd2d7b294e36a496a6a1b
Paige Wiener <snowplushie@yahoo.com>
2019-03-13 07:47:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,11 +20,13 @@ class CashRegister
   end
   
   def apply_discount(price)
-     if @discount == 0
+     if @discount > 0
+      @discount_amount = (self.total * @discount)/100
+      self.total -= @discount_amount
+      return "After the discount, the total comes to $#{@total}."
+    else
       return "There is no discount to apply."
     end
-    @total -= @total * @discount / 100
-    return "After the discount, the total comes to $#{@total}."
   end 
   
   def void_last_transaction
